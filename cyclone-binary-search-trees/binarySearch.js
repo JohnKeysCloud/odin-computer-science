21624176253c47b5dcf0105b7022f9edd3fded4a
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-06-20 15:38:02 -0400

    add notes to binary search recursive implementation

--- cyclone-binary-search-trees/binarySearch.js
+++ cyclone-binary-search-trees/binarySearch.js
@@ -4,20 +4,28 @@
 let testArray = [1, 4, 8, 9, 32, 55, 73];
 
 function binarySearchRecursive(sortedArray, value) {
+  if (sortedArray.length === 0) return false; // * {1}
+
   let middleIndex = Math.floor(sortedArray.length / 2);
 
   if (value === sortedArray[middleIndex]) return true;
 
   if (value < sortedArray[middleIndex]) {
-    return binarySearch(sortedArray.slice(0, middleIndex), value);
+    return binarySearchRecursive(sortedArray.slice(0, middleIndex), value);
   } else if (value > sortedArray[middleIndex]) {
-    return binarySearch(sortedArray.slice(middleIndex + 1), value);
+    return binarySearchRecursive(sortedArray.slice(middleIndex + 1), value);
   }
 
   return false;
 }
 // console.log(binarySearchRecursive(testArray, 33)); // False
 // console.log(binarySearchRecursive(testArray, 32)); // True
+
+// * {1} The base case is explicitly written for readability; However, the function would run properly without it. If the array is empty because we haven't found our value after slicing the array up into a billion pieces, the `middleIndex` calculation results in our value being `0`. The value at index 0 of an empty array is undefined. Therefore all conditionals of the function are bypassed and `false` is returned.
+
+// ? Exhibit A:
+let array = [];
+// console.log(array[0]); // undefined
 
 // 💭 --------------------------------------------------------------
 
@@ -27,12 +35,13 @@ function binarySearchIterative(sortedArray, value) {
 
     if (sortedArray[middle] === value) return true;
 
-    if (value > sortedArray[middle]) {
-      sortedArray = sortedArray.slice(middle + 1);
-    } else if (value < sortedArray[middle]) {
+    if (value < sortedArray[middle]) {
       sortedArray = sortedArray.slice(0, middle);
+    } else if (value > sortedArray[middle]) {
+      sortedArray = sortedArray.slice(middle + 1);
     }
   }
+
   return false
 }
 // console.log(binarySearchIterative(testArray, 33));
